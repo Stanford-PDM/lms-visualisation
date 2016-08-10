c3f942222d16f3285a79779fd3831a8805010a08
Damien Engels <engelsdamien@gmail.com>
2016-08-10 16:35:48 -0700

    Move the limit to 100 columns

--- format.sh
+++ format.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+set -x
 
-find . -name '*.scala' | xargs -n 1 -J %% scalafmt --style "Scala.js" --in-place --files %%
+find . -name '*.scala' | xargs -t -n 1 -J %% scalafmt --style "Scala.js" --maxColumn 100 --in-place --files %%
