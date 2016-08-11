04616a218d37c4f559b72deca5ef79b713cd8cfb
Damien Engels <engelsdamien@gmail.com>
2016-08-10 17:24:10 -0700

    Add js interface to get information about symbols

--- js/src/main/scala/org/lmsviz/Main.scala
+++ js/src/main/scala/org/lmsviz/Main.scala
@@ -12,6 +12,7 @@ object Main extends js.JSApp {
   }
 
   import model.lms._
+
   val trace = {
     import io.circe.generic.auto._
     import io.circe.parser._
@@ -23,5 +24,21 @@ object Main extends js.JSApp {
       println(error.toString)
       sys.error("Cannot parse json ast")
     }.merge
+  }
+
+  val stmMap = trace.fullGraph.map(stm => stm.id -> stm).toMap
+
+}
+
+// Js interface to retrieve symbols
+@JSExport("DB")
+object DB {
+
+  import js.JSConverters._
+
+  @JSExport
+  def getInfo(id: Int) = {
+    val stm = Main.stmMap(id)
+    Map("repr" -> stm.repr, "info" -> stm.comments.toJSDictionary).toJSDictionary
   }
 }
